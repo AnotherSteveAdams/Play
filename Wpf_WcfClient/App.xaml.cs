e2804af39b788407c57bbdce01c567ccfef6056e
Steve <ste_adams@yahoo.co.uk>
2014-05-06 15:51:42 +0100

    Trying out the faultnotifcation callback...works in part.

--- Wpf_WcfClient/App.xaml.cs
+++ Wpf_WcfClient/App.xaml.cs
@@ -13,5 +13,33 @@ namespace Wpf_WcfClient
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnLoadCompleted(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnLoadCompleted(e);
+
+            // Add the event handler for handling UI thread exceptions to the event.
+            // Application.ThreadException += new ThreadExceptionEventHandler(ExceptionDialog.UIThreadException);
+            Application.Current.DispatcherUnhandledException += Current_DispatcherUnhandledException;
+            // Application.Current.
+            //// Set the unhandled exception mode to force all Windows Forms errors to go through
+            //// our handler.
+            //Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            //// Add the event handler for handling non-UI thread exceptions to the event. 
+            //AppDomain.CurrentDomain.UnhandledException +=
+            //    new UnhandledExceptionEventHandler(MyApp.CurrentDomain_UnhandledException);
+        }
+
+        void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            // throw new NotImplementedException();
+            
+        }
+
+        void Current_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            // throw new NotImplementedException();
+        }
     }
+    
 }
--- Wpf_WcfClient/MainWCFTestVM.cs
+++ Wpf_WcfClient/MainWCFTestVM.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.ServiceModel;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using WcfServiceLibrary1;
 
@@ -24,23 +25,59 @@ namespace Wpf_WcfClient
         public MainWCFTestVM()
         {
 
+            CreateChannel();
+
+        }
+        void CreateChannel()
+        {
             var binding = new NetTcpBinding(SecurityMode.None);
-            binding.SendTimeout = new TimeSpan(0, 0, 0, 30);
-            binding.ReceiveTimeout = TimeSpan.MaxValue;
-            var factory = new DuplexChannelFactory<IService1>(
+            binding.SendTimeout = new TimeSpan(0, 0, 0, 3);
+            binding.ReceiveTimeout = new TimeSpan(0, 0, 0, 3); ;// TimeSpan.MaxValue;
+            binding.OpenTimeout = new TimeSpan(0, 0, 0, 3);
+            binding.CloseTimeout = new TimeSpan(0, 0, 0, 3); 
+            _factory = new DuplexChannelFactory<IService1>(
                     this,
                     binding,
                     "net.tcp://SteveX1:9966/TheWCFService/IService1"
                 );
-            c = factory.CreateChannel();
-            c.Subscribe();
-            
+            _myservice = _factory.CreateChannel();
 
+            ((ICommunicationObject)_myservice).Faulted += MainWCFTestVM_Faulted;
+
+            try
+            {
+                _myservice.Subscribe();
+            }
+            catch (EndpointNotFoundException ex)
+            {
+
+            }
+            StatusText = "Connected";
+            ServiceUp = true;
+            OnPropertyChanged("ServiceUp");
         }
-
+        DuplexChannelFactory<IService1> _factory;
+        void MainWCFTestVM_Faulted(object sender, EventArgs e)
+        {
+            StatusText = "Faulted";
+            ServiceUp = false;
+            OnPropertyChanged("ServiceUp");
+            ((ICommunicationObject)sender).Abort();
+            if (sender is IService1)
+            {
+                System.Threading.Thread.Sleep(500);
+                System.Windows.Application.Current.Dispatcher.BeginInvoke(new Action(() => CreateChannel()), null);
+                
+            }
+        }
+        public string StatusText
+        {
+            get { return _statusText; }
+            set { _statusText = value; OnPropertyChanged("StatusText"); }
+        }
         ~MainWCFTestVM()
         {
-            c.Unsubscribe();
+            _myservice.Unsubscribe();
         }
 
         public string Message
@@ -65,13 +102,18 @@ namespace Wpf_WcfClient
             }
         }
 
+        public bool ServiceUp
+        {
+            get;
+            set;
+        }
         public bool InError
         {
             get;
             set;
         }
 
-        IService1 c;
+        IService1 _myservice;
         ICommand _myCommand;
         public ICommand CommandSendPrice
         {
@@ -81,16 +123,39 @@ namespace Wpf_WcfClient
                 {
                     _myCommand = new RelayCommand(p =>
                     {
+                        if (Message.Contains("Jacob"))
+                        {
+                            ErrorMessage = "You are a burglar, system will self destruct in 5 seconds";
+
+                            Task.Run(() =>
+                                {
+                                    for (int timeToGo = 5; timeToGo >= 2; timeToGo -= 1)
+                                    {
+                                        System.Threading.Thread.Sleep(1000);
+                                        ErrorMessage = "You are a burglar, system will self destruct in " + timeToGo + " seconds";
+                                    }
+
+                                    System.Threading.Thread.Sleep(1000);
+                                    ErrorMessage = "Self destruct deactivated, sorry I thougth you were trying to steal me !";
+
+                                });
+                            return;
+                        }
                         if (Message.Contains("Bloody"))
                         {
                             ErrorMessage = "Don't say the word Bloody, okay !  Message was not sent";
                             return;
                         }
-                        c.PublishPriceChange(Message, 123, 32);
+                        _myservice.PublishPriceChange(Message, 123, 32);
                     });
                 }
                 return _myCommand;
             }
+        }
+
+        void t_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
         public ObservableCollection<string> _theList = new ObservableCollection<string>();
@@ -110,6 +175,7 @@ namespace Wpf_WcfClient
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
+        private string _statusText;
         private void test()
         {
             
